4a1ce111038dbd42836b2e0bf9cf35ed1dc5c54f
Xiaobing Zhu <Hello16369@163.com>
2026-01-15 09:10:31 +0800

    chore: init commit

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+    {
+        type: 'lib',
+        typescript: true,
+        ignores: [
+            '**/dist',
+            '**/node_modules',
+            '**/*.d.ts',
+        ],
+    },
+    {
+        rules: {
+            'no-console': 'off',
+            'node/prefer-global/process': 'off',
+            'node/prefer-global/buffer': 'off',
+            'ts/consistent-type-definitions': 'off',
+        },
+    },
+)
